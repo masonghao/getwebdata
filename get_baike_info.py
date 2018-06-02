ee0d5aceb9d5c8931734ec330835e9416cbef85d
masonghao <30997918+masonghao@users.noreply.github.com>
2018-06-02 23:08:26 +0800

    change some code
    123

--- get_baike_info.py
+++ get_baike_info.py
@@ -1,6 +1,6 @@
-import urllib.request
+import urllib.request, re
 from bs4 import BeautifulSoup
-
+from lib.mshlib import getFileContent,putFileContent
 # 获取一个网页内容并本地存档
 def getHtml(url, fileName):
     htmlCode1 = urllib.request.urlopen(url)
@@ -14,26 +14,18 @@ def parserHtml(fileName):
     fileCode = getFileContent(fileName)
 
     soup = BeautifulSoup(fileCode, 'html.parser')
-    links = soup.find_all('a')
-    for a in links:
-        print(a.name, a['href'], a.get_text())
+    h1 = soup.find('h1').get_text()
+    links = soup.find_all('a', href=re.compile(r'/item/.+'))
+    linksset = set()
+    for link in links:
+        linksset.add('https://baike.baidu.com/'+link['href'])
+        print(link.name, link.get_text(), link['href'])
 
-# 获取本地文件的内容到字符串
-def getFileContent(fileName, mods = 'rb'):
-    file = open(fileName, mods)
-    fileCode = file.read()
-    file.close()
-    return fileCode
-
-# 输出字符串到一个本地文件
-def putFileContent(fileName, filecode, mods = 'wb'):
-    f = open(fileName, mods)
-    lens = f.write(filecode)
-    f.close()
-    return lens
+    print('百科标题：%s' % h1)
+    print(linksset)
 
 if __name__ == '__main__':
-    url='http://localhost:8000/article/'
+    url='https://baike.baidu.com/item/html5'
     fileName = 'data/msong.html'
-    getHtml(url, fileName)
+    # getHtml(url, fileName)
     parserHtml(fileName)
